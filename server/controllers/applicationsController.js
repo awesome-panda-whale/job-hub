f90e8ba26dfca3c4751e34e59c85acd8056dfa62
Mingzhu Wan <mingzhuwan@gmail.com>
2024-06-19 09:37:51 +1000

    get changes from dev

--- server/controllers/applicationsController.js
+++ server/controllers/applicationsController.js
@@ -2,63 +2,62 @@ const db = require('../models/userModels');
 
 const applicationController = {};
 
-//already change the createApp middleware - Aria
-applicationController.createApp = async(req, res, next) => {
-  const { company, position, url, date_applied, status_id, contact, email, notes} = req.body;
-  console.log(req.body);
-  if (!company || !position ) {
-    return next({
-      log:'Company and position should not be empty',
-      message: 'Company and position should not be empty'
-    })
-  } 
-
-  const params = [company, position, url, date_applied, status_id, contact, email, notes];
-  const query = `
-        INSERT INTO Applications 
-        (company, position, url, date_applied, status_id, contact, email, notes)
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-        RETURNING company, position`
-   
+applicationController.createApp = async (req, res, next) => {
   try {
-    const dataReturn = await db.query(query, params);
-    console.log(dataReturn.rows[0]);
-    res.locals.createApp = {
-      success: true,
-      message: "Saving Application",
-      data: dataReturn.rows[0]
-    };
-    return next();
-  } catch(err) {
+    const { company, position, url, status_id, contact, email, notes } = req.body;
+    // For testing purposes, hardcoded user_id. This should come from the logged-in user session.
+    
+    const user_id = 2;
+
+    const params = [company, position, url, status_id, contact, email, notes];
+    const query = `INSERT INTO applications (company, position, url, status_id, contact, email, notes) VALUES ($1, $2, $3, $4, $5, $6, $7) returning id`;
+
+    const applicationResult = await db.query(query, params);
+    if (applicationResult.rows.length > 0) {
+      console.log('Create application, ', applicationResult);
+      const application_id = applicationResult.rows[0].id;
+      const parameters = [user_id, application_id];
+      const user_app_query = 'INSERT INTO Users_Applications (user_id, application_id) VALUES ($1, $2) returning application_id';
+      const linkResult = await db.query(user_app_query, parameters);
+
+      console.log('Create link between user and application,', linkResult);
+      res.locals.application = linkResult.rows[0].application_id;
+      return next();
+    } else {
+      throw new Error('Application creation failed');
+    }
+  } catch (err) {
+    console.log(err);
     return next({
-      error: err,
-      message: 'Error in Saving your Application',
+      log: 'Error in applicationController.createApp',
+      status: 500,
+      message: { err: 'An error occurred while creating the application' },
     });
   }
 };
 
 applicationController.getApp = async (req, res, next) => {
-  const { id } = req.params;
-  const query = `SELECT a.id, a.company, a.date_applied, a.status, a.role, users.username
-    FROM applications a INNER JOIN users 
-    ON applications.user_id = users.id
-    WHERE applications.id = $1`;
-  const params = [id];
+  try {
+    const { id } = req.params;
+    const query = `
+      SELECT a.id, a.company, a.position, a.url, a.date_applied, a.status_id, a.contact, a.email, a.notes, u.username
+      FROM applications a
+      INNER JOIN users u ON a.user_id = u.id
+      WHERE a.id = $1`;
+    const params = [id];
 
-  await db.query(query, params)
-    .then((data) => {
-      console.log('Get application, ', data);
-      res.status(200).send('Get App');
-      res.locals.getApp = data.rows[0];
-      return next();
-    })
-    .catch((err) => {
-      console.log(err);
-      return next({
-        error: err,
-        message: 'Error in applicationController.getApp',
-      });
+    const data = await db.query(query, params);
+    console.log('Get application, ', data);
+    res.locals.getApp = data.rows[0];
+    return next();
+  } catch (err) {
+    console.log(err);
+    return next({
+      log: 'Error in applicationController.getApp',
+      status: 500,
+      message: { err: 'An error occurred while retrieving the application' },
     });
+  }
 };
 
-module.exports = applicationController
+module.exports = applicationController;
